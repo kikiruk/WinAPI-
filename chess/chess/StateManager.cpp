c5dda74ce436f62a100e7002fad21964a8a79789
seagull <kikiruk123@naver.com>
2023-05-09 18:29:44 +0900

    필요없는 파일 삭제
    필요없는 파일 StateManager.cpp 삭제후 잘못 적혀있는 이름 StatrManager.cpp 를 StateManager.cpp 로 수정

--- chess/chess/StateManager.cpp
+++ chess/chess/StateManager.cpp
@@ -1,12 +1,34 @@
 #include "StateManager.h"
 
+State* StateManager::StateNow = nullptr;
+Main* StateManager::_main = nullptr;
+Option* StateManager::option = nullptr;
+GameStart* StateManager::gameStart = nullptr;
+BeforGameStart* StateManager::beforGameStart = nullptr;
+Game_Clear* StateManager::game_Clear = nullptr;
 
-
-StateManager::StateManager()
+Main* StateManager::getMainState()
 {
+	return _main;
 }
 
 
-StateManager::~StateManager()
+GameStart* StateManager::getGameStartState()
 {
+	return gameStart;
+}
+
+Option* StateManager::getOptionState()
+{
+	return option;
+}
+
+BeforGameStart* StateManager::getBeforGameStartState()
+{
+	return beforGameStart;
+}
+
+Game_Clear* StateManager::getGame_Clear()
+{
+	return game_Clear;
 }
